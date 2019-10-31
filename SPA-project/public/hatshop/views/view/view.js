e85dceb1884105ed51232a57b3b75364a1f2d89f
clementb49 <clement.boussiron@gmail.com>
2019-10-31 05:59:48 +0100

    adding view.js file of the seminar 4

---
+++ SPA-project/public/hatshop/views/view/view.js
@@ -0,0 +1,42 @@
+var View = {};
+View.renderer = {};
+View.loadTemplate = function (filename) {
+    return $.ajax({
+        url: '/webapp/views/templates/' + filename + '.hbs'
+    });
+}
+View.renderTemplate = function (id, container, context) {
+    return View.loadTemplate(id)
+        .then(function (source) {
+            var template = Handlebars.compile(source);
+            var html = template(context);
+            return $('#' + container).html(html)
+        })
+}
+
+View.loadPartial = function (filename) {
+    return $.ajax({
+        url: '/webapp/views/partials/' + filename + '.hbs'
+    })
+        .then(function (contents) {
+            return Handlebars.registerPartial(filename, contents);
+        });
+}
+
+$(function () {
+    window.addEventListener('popstate', (event) => Controller.
+        router.route(), false);
+    var promises = [View.loadPartial('book-partial')];
+    Promise.all(promises)
+        .then(function () {
+
+
+            Model.getBooks()
+                .then(function (books) {
+                    // Controller.controllers.index.refresh()
+                    Controller.router.route('Page no found!');
+                })
+
+        });
+
+})
