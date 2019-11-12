0a3b726ac3c114f15aec24429b15d201a6e497be
clementb49 <clement.boussiron@gmail.com>
2019-11-12 14:44:09 +0100

    create folder structure for API

--- SPA-project-with-api/app.js
+++ SPA-project-with-api/app.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var viewRouter = require('./routes/view');
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api')
+const viewRouter = require('./routes/view');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,7 +16,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/hatshop/views', viewRouter);
-
+app.use('/api', apiRouter)
 module.exports = app;
